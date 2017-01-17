9d35fd84807044a9bdde1dc615f9fb8024f6b576
이창주 <sicko@naver.com>
2017-01-18 08:56:29 +0900

    섯다게임 수정

--- SDgame2/src/com/sd/game/Exam.java
+++ SDgame2/src/com/sd/game/Exam.java
@@ -15,10 +15,55 @@ public class Exam {
 		player1.name = scanner.nextLine();
 		System.out.println("2 플레이어 이름을 입력하세요.");
 		player2.name = scanner.nextLine();
+		
+		System.out.print(player1.name + "님 숫자를 입력하세요.");
+		int select;
 		player1.input();
-		player1.solution();
+		while (true) {
+			player1.select = scanner.nextInt();
+		if (player1.select < 1 || player1.select > 10) {
+			System.out.print("잘못 입력하셨습니다. 숫자를 다시 입력하세요 ! \n");
+			continue;
+		}else break;
+		}
+		System.out.print(player2.name + "님 숫자를 입력하세요.");
 		player2.input();
+		while (true) {
+			player2.select = scanner.nextInt();
+		if (player2.select < 1 || player2.select > 10) {
+			System.out.print("잘못 입력하셨습니다. 숫자를 다시 입력하세요 ! \n");
+			continue;
+		}else break;
+		}
+		System.out.print(player1.name + "의 첫 번째 패 숫자 = " + player1.select + "     ");
+		System.out.println(player1.name + "의 랜덤 패 숫자= " + player1.random);
+		
+		System.out.print(player2.name + "의 첫 번째 패 숫자 = " + player2.select + "     ");
+		System.out.println(player2.name + "의 랜덤 패 숫자= " + player2.random);
+		
+		
+		
+		player1.solution();
+		if (player1.res >= 21){
+		System.out.println("38광땡!!!!!");
+		}
+		else if(player1.res >10){
+		System.out.println(player1.random + "땡");
+		}
+		else{
+		System.out.println(player1.res + "끗");
+		}
 		player2.solution();	
+		if (player2.res >= 21){
+			System.out.println("38광땡!!!!!");
+			}
+			else if(player2.res >10){
+			System.out.println(player2.random + "땡");
+			}
+			else{
+			System.out.println(player2.res + "끗");
+			}
+		
 		player1.compare(player2);
 		player2.compare(player1);
 		 
@@ -28,13 +73,15 @@ public class Exam {
 				System.out.println("************" +player2.name+"님 축하합니다. 승리 하셨습니다 ************");
 			else if (player1.score == player2.score)
 				System.out.println("************ 아쉽네요. 무승부입니다. ************");
-	
-			System.out.println("계속하시겠습니까? (y/n)");
-			scanner.nextLine();
-			if (scanner.nextLine().equals("n")) {
-				System.out.println("게임이 종료 되었습니다.");
-				break;}
-		}
-	}
+		 
+         System.out.println("계속하시겠습니까? (y/n)");
+         scanner.nextLine();
+         
+         if (scanner.nextLine().equals("n")) {
+        	 System.out.println("게임이 종료 되었습니다.");
+        	 break;}        
+        }
+
+   }
 
 }
--- SDgame2/src/com/sd/game/SDgame.java
+++ SDgame2/src/com/sd/game/SDgame.java
@@ -1,5 +1,4 @@
 package com.sd.game;
-
 import java.util.Scanner;
 
 public class SDgame {
@@ -8,9 +7,7 @@ public class SDgame {
 	int select;
 	int random;
 	int score;
-
-	Scanner scanner = new Scanner(System.in);
-
+	
 	public SDgame() {
 		name = " ";
 		select = 0;
@@ -19,34 +16,20 @@ public class SDgame {
 	}
 
 	void input() {
-		random = ((int) (Math.random() * 10) + 1);
-		System.out.print(name + "님 숫자를 입력하세요.");
-		while (true) {
-			select = scanner.nextInt();
-			if (select < 1 || select > 10) {
-				System.out.print("잘못 입력하셨습니다. A 숫자를 다시 입력하세요 ! \n");
-				continue;
-			} // A입력 오류 검사
-			break;
-		}
-		System.out.print(name + "의 첫 번째 패 숫자 = " + select + "     ");
-		System.out.println(name + "의 랜덤 패 숫자= " + random);
-
-	}// input 메소드
+	random = ((int) (Math.random() * 10) + 1);
+	this.select = select;
+	}	
 
 	void solution() {
 		if (select == 3 & random == 8 || select == 8 & random == 3) {
-			System.out.println("38광땡!!!!!");
 			res = 21;
 		} else if (select == random) {
-			System.out.println(random + "땡");
 			res = 10 + random;
 		} else {
 			res = select + random;
 			if (res > 10) {
 				res = res - 10;
 			}
-			System.out.println(res + "끗");
 		}
 	}
 
@@ -58,4 +41,4 @@ public class SDgame {
 		if (res == player.res) 
 			score =score;
 	}
-	}// class
+}// class
