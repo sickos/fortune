e8b2eeb74712f6844893e9609ddbaa4285ae7bb7
기호 <david9921@naver.com>
2017-01-18 14:47:42 +0900

    private 필드 get,set 추가

--- SDgame2/src/com/sd/game/Exam.java
+++ SDgame2/src/com/sd/game/Exam.java
@@ -10,74 +10,74 @@ public class Exam {
 			SDgame player1 = new SDgame();
 			SDgame player2 = new SDgame();	
 			System.out.println("1 플레이어 이름을 입력하세요.");
-			player1.name = scanner.nextLine();
+			player1.setName(scanner.nextLine());
 			System.out.println("2 플레이어 이름을 입력하세요.");
-			player2.name = scanner.nextLine();
+			player2.setName(scanner.nextLine());
 	
 			for(int i=0;i<5;i++){
 				
-			System.out.print("\n"+player1.name + "님 숫자를 입력하세요.");
+			System.out.print("\n"+player1.getName() + "님 숫자를 입력하세요.");
 			player1.input();
 			while (true) {
-				player1.select = scanner.nextInt();
-				if (player1.select < 1 || player1.select > 10) {
+				player1.setSelect(scanner.nextInt());
+				if (player1.getSelect() < 1 || player1.getSelect() > 10) {
 					System.out.print("잘못 입력하셨습니다. 숫자를 다시 입력하세요 ! \n");
 					continue;
 				} else
 					break;}
 			
-			System.out.print(player2.name + "님 숫자를 입력하세요.");
+			System.out.print(player2.getName() + "님 숫자를 입력하세요.");
 			player2.input();
 			while (true) {
-				player2.select = scanner.nextInt();
-				if (player2.select < 1 || player2.select > 10) {
+				player2.setSelect(scanner.nextInt());
+				if (player2.getSelect() < 1 || player2.getSelect() > 10) {
 					System.out.print("잘못 입력하셨습니다. 숫자를 다시 입력하세요 ! \n");
 					continue;
 				} else
 					break;}
 			
-			System.out.print("\n"+player1.name + "의 첫 번째 패 숫자 = " + player1.select + "     ");
-			System.out.println(player1.name + "의 랜덤 패 숫자= " + player1.random);
+			System.out.print("\n"+player1.getName() + "의 첫 번째 패 숫자 = " + player1.getSelect() + "     ");
+			System.out.println(player1.getName() + "의 랜덤 패 숫자= " + player1.getRandom());
 
-			System.out.print(player2.name + "의 첫 번째 패 숫자 = " + player2.select + "     ");
-			System.out.println(player2.name + "의 랜덤 패 숫자= " + player2.random);
+			System.out.print(player2.getName() + "의 첫 번째 패 숫자 = " + player2.getSelect() + "     ");
+			System.out.println(player2.getName() + "의 랜덤 패 숫자= " + player2.getRandom());
 
 			res1 = player1.solution();
 			if (res1 >= 21) {
-				System.out.println("\n"+player1.name + "님 38광땡!!!!!");
+				System.out.println("\n"+player1.getName() + "님 38광땡!!!!!");
 			} else if ( res1>10  )  {
-				System.out.println("\n"+player1.name+ "님 " + player1.random + "땡");
+				System.out.println("\n"+player1.getName()+ "님 " + player1.getRandom() + "땡");
 			} else {
-				System.out.println("\n"+player1.name+ "님 " + res1 + "끗");
+				System.out.println("\n"+player1.getName()+ "님 " + res1 + "끗");
 			}
 			
 			res2 = player2.solution();
 			if (res2 >= 21) {
-				System.out.println(player2.name + "님 38광땡!!!!!");
+				System.out.println(player2.getName() + "님 38광땡!!!!!");
 			} else if ( res2>10) {
-				System.out.println(player2.name+ "님 " + player2.random + "땡");
+				System.out.println(player2.getName()+ "님 " + player2.getRandom() + "땡");
 			} else {
-				System.out.println(player2.name+ "님 " + res2 + "끗");
+				System.out.println(player2.getName()+ "님 " + res2 + "끗");
 			}
 			
 			if(res1 > res2)
-				System.out.println("************ "+ (i+1) +" 라운드 " + player1.name + "님이 승리 하셨습니다 ************");
+				System.out.println("************ "+ (i+1) +" 라운드 " + player1.getName()+ "님이 승리 하셨습니다 ************");
 			else if(res1 < res2)
-				System.out.println("************ "+ (i+1) +" 라운드 " + player2.name + "님이 승리 하셨습니다 ************");
+				System.out.println("************ "+ (i+1) +" 라운드 " + player2.getName() + "님이 승리 하셨습니다 ************");
 			else
 				System.out.println("************************** "+ (i+1) +" 라운드 무승부 입니다. **************************");
 			player1.compare(player2);
 			 }// 5판 반복
 		
 		
-			System.out.println(player1.name+" 님 최종  "+ player1.score + " 점 입니다." );
-			System.out.println(player2.name+" 님 최종  "+ player2.score + " 점 입니다." );
+			System.out.println(player1.getName()+" 님 최종  "+ player1.getScore() + " 점 입니다." );
+			System.out.println(player2.getName()+" 님 최종  "+ player2.getScore() + " 점 입니다." );
 			
-			if (player1.score > player2.score)
-				System.out.println("************" + player1.name + "님 축하합니다. 최종 승리 하셨습니다 ************");
-			else if (player1.score < player2.score)
-				System.out.println("************" + player2.name + "님 축하합니다. 최종 승리 하셨습니다 ************");
-			else if (player1.score == player2.score)
+			if (player1.getScore() > player2.getScore())
+				System.out.println("************" + player1.getName() + "님 축하합니다. 최종 승리 하셨습니다 ************");
+			else if (player1.getScore() < player2.getScore())
+				System.out.println("************" + player2.getName() + "님 축하합니다. 최종 승리 하셨습니다 ************");
+			else if (player1.getScore() == player2.getScore())
 				System.out.println("************************** 아쉽네요. 최종 무승부입니다. **************************");
 
 			System.out.println("계속하시겠습니까? (y/n)");
--- SDgame2/src/com/sd/game/SDgame.java
+++ SDgame2/src/com/sd/game/SDgame.java
@@ -1,12 +1,52 @@
 package com.sd.game;
 
 public class SDgame {
-	int res;
-	String name;
-	int select;
-	int random;
-	int score;
+	private int res;
+	private String name;
+	private int select;
+	private int random;
+	private int score;
 	
+	public int getRes() {
+		return res;
+	}
+
+	public void setRes(int res) {
+		this.res = res;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSelect() {
+		return select;
+	}
+
+	public void setSelect(int select) {
+		this.select = select;
+	}
+
+	public int getRandom() {
+		return random;
+	}
+
+	public void setRandom(int random) {
+		this.random = random;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
 	public SDgame() {
 		name = " ";
 		select = 0;
